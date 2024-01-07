7e4894533d7e33c8ba6006a10c804c6537a77caa
szary <szary@tenteg.us>
2024-01-07 22:57:51 +0100

    First commint for school project

---
+++ Client/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System.Net.Sockets;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj adres serwera: ");
+            string ip = Console.ReadLine();
+            
+            
+            string[] table = new string[9];
+            for (int i = 0; i <= 8; i++) { table[i] = $"{i + 1}"; }
+            
+            while (true)
+            {
+                Console.Write($"{table[0]} | {table[1]} | {table[2]}\n---------\n{table[3]} | {table[4]} | {table[5]}\n---------\n{table[6]} | {table[7]} | {table[8]}");
+            }
+                
+            
+            
+
+            // connection:
+            // try
+            // {
+            //     while (true)
+            //     {
+            //         TcpClient client = new TcpClient("127.0.0.1", 2137);
+            //         
+            //         string messageToSend = Console.ReadLine();
+            //         int byteCount = Encoding.ASCII.GetByteCount(messageToSend);
+            //         byte[] sendData = new byte[byteCount];
+            //         sendData = Encoding.ASCII.GetBytes(messageToSend);
+            //
+            //         NetworkStream stream = client.GetStream();
+            //         stream.Write(sendData, 0, sendData.Length);
+            //     }
+            // }
+            // catch (Exception e)
+            // {
+            //     goto connection;
+            // }
+        }
+    }
+}
+
---
+++ Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.Encodings;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 2137);
+            listener.Start();
+            while (true) 
+            {
+                Console.Write('>');
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+                    int recv = 0;
+                    foreach (byte b in buffer)
+                    {
+                        if (b != 0)
+                        {
+                            recv++;
+                        }
+                    }
+
+                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                    Console.WriteLine(request);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+    }
+}
+
